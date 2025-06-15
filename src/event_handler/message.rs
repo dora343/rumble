14e5dd4ff006a230de009c1013a97073746ebf18
dora343 <wusiukwan31623159@gmail.com>
2025-06-15 17:23:16 +0800

    use MessageBuilder for building mentions, reorg match expression

--- src/event_handler/message.rs
+++ src/event_handler/message.rs
@@ -1,4 +1,4 @@
-use crate::{serenity, Data};
+use crate::{Data, serenity};
 pub mod hello;
 use hello::respond_hello;
 
@@ -9,19 +9,26 @@ pub mod get_pinged;
 use get_pinged::respond_get_pinged;
 
 use serenity::model::channel::Message;
+use serenity::utils::MessageBuilder;
 type Error = Box<dyn std::error::Error + Send + Sync>;
 
-pub async fn message_handler(ctx: &serenity::Context, data: &Data, msg: &Message) -> Result<(), Error> {
+pub async fn message_handler(
+    ctx: &serenity::Context,
+    data: &Data,
+    msg: &Message,
+) -> Result<(), Error> {
     if msg.author.id == ctx.cache.current_user().id {
         return Ok(());
     }
-    
-    let ping_bot = format!("<@{}>", ctx.cache.current_user().id);
 
-    match &msg.content {
-        _ if msg.content.contains("hello") => respond_hello(ctx, msg).await,
-        _ if msg.content.eq("dora") => respond_ping_dora(ctx, msg).await,
-        _ if msg.content.contains(ping_bot.as_str()) => respond_get_pinged(ctx, msg).await,
-        _ => Ok(()),
+    let ping_bot = MessageBuilder::new()
+        .mention(&ctx.cache.current_user().id)
+        .build();
+    
+    match msg {
+        msg if msg.content.eq("dora") => respond_ping_dora(ctx, msg).await,
+        msg if msg.content.contains("hello") => respond_hello(ctx, msg).await,
+        msg if msg.content.contains(&ping_bot) => respond_get_pinged(ctx, msg).await,
+        _ => Ok(())
     }
 }
