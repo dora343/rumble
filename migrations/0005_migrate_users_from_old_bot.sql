240ca3dad700f3f48cfb7b5273783e4eb22e292f
dora343 <wusiukwan31623159@gmail.com>
2025-06-21 10:32:35 +0800

    add users data migration patch from the old bot

---
+++ migrations/0005_migrate_users_from_old_bot.sql
@@ -0,0 +1,47 @@
+insert into gamble.users (id, tokens)
+values (508623705916571649, 678901);
+
+insert into gamble.user_stat (id, max_tokens)
+values (508623705916571649, 678901);
+
+insert into gamble.users (id, tokens)
+values (458866430881038346, 30749);
+
+insert into gamble.user_stat (id, max_tokens)
+values (458866430881038346, 30749);
+
+insert into gamble.users (id, tokens)
+values (968856913599139921, 102000);
+
+insert into gamble.user_stat (id, max_tokens)
+values (968856913599139921, 102000);
+
+insert into gamble.users (id, tokens)
+values (242543304418394124, 10000);
+
+insert into gamble.user_stat (id, max_tokens)
+values (242543304418394124, 10000);
+
+insert into gamble.users (id, tokens)
+values (445943313384865813, 10000);
+
+insert into gamble.user_stat (id, max_tokens)
+values (445943313384865813, 10000);
+
+insert into gamble.users (id, tokens)
+values (355370319944482816, 10000);
+
+insert into gamble.user_stat (id, max_tokens)
+values (355370319944482816, 10000);
+
+insert into gamble.users (id, tokens)
+values (997878937210323015, 7300);
+
+insert into gamble.user_stat (id, max_tokens)
+values (997878937210323015, 7300);
+
+insert into gamble.users (id, tokens)
+values (400941378395439104, 103021);
+
+insert into gamble.user_stat (id, max_tokens)
+values (400941378395439104, 103021);
