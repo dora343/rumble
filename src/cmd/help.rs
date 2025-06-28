c7348fba819fe003a4d918fcb6e8c78236f42622
dora343 <wusiukwan31623159@gmail.com>
2025-06-28 14:37:26 +0800

    add basic help cmd

--- src/cmd/help.rs
+++ src/cmd/help.rs
@@ -1,3 +1,5 @@
+use serenity::all::MessageBuilder;
+
 use crate::cmd::Context;
 use crate::cmd::Error;
 
@@ -5,8 +7,36 @@ use crate::cmd::Error;
 #[poise::command(prefix_command, aliases("h"))]
 pub async fn help(ctx: Context<'_>) -> Result<(), Error> {
     // obtain the author as user if not specified
-    let response: String = "This feature is disabled, for now.".into();
-    println!("{}", response);
+    let response: String = MessageBuilder::new()
+        .push_line("# Available Commands:")
+        // .push_line("```rust")
+        .push_line("")
+        .push_line("### Help Command")
+        .push_line("Command: `.help`")
+        .push_line("Alias: `.h`")
+        .push_line("### Register your account")
+        .push_line("Command: `.register`")
+        .push_line("Alias: `.reg`")
+        .push_line("### Gamble with your tokens")
+        .push_line("Command: `.gamble <your bet>`")
+        .push_line("Alias: `.g <your bet>`")
+        .push_line("### Toggle auto revive")
+        .push_line("Command: `.gamble autorevive`")
+        .push_line("Alias: `.g autorevive`")
+        .push_line("### Display statistics")
+        .push_line("Command: `.gamble statistics`")
+        .push_line("Alias: `.g stat`")
+        .push_line("### Use all your tokens as bet")
+        .push_line("Command: `.gamble allin`")
+        .push_line("Alias: `.g all`")
+        .push_line("### Display leaderboard")
+        .push_line("Command: `.gamble leaderboard`")
+        .push_line("Alias: `.g lb`")
+        .push_line("### Revive")
+        .push_line("Command: `.revive`")        
+        // .push_line("```")        
+        .build();
+    // println!("{}", response);
     ctx.say(response).await?;
     Ok(())
 }
